1d04b1b2317425a291f33187eb6a5b6de7c4886e
Anderson E. Peñaloza C <designeromega@gmail.com>
2021-11-20 17:39:23 +0100

    moved folder from app to pages folder

--- src/pages/folder/folder.page.ts
+++ src/pages/folder/folder.page.ts
@@ -4,15 +4,15 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-folder',
   templateUrl: './folder.page.html',
-  styleUrls: ['./folder.page.scss'],
+  styleUrls: ['./folder.page.scss']
 })
 export class FolderPage implements OnInit {
   public folder: string;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute) {}
 
   ngOnInit() {
-    this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+    this.folder =
+      this.activatedRoute.snapshot.paramMap.get('id');
   }
-
 }
